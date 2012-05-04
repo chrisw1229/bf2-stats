75fd3853d90cb022b92830cc8b80073b3fb8e5b3
Clinton Jenkins <cjenkins@robowarp.com>
2012-05-03 23:07:30 -0400

    Removed compiled python files and bug fixes for cherry py configuration.  Added current_dir to application.py and referenced that variable in application.conf.  Also imported statparser in application.py so that it was available for engine configuration in application.conf.

--- webapp/application.py
+++ webapp/application.py
@@ -3,6 +3,7 @@ import os.path
 import cherrypy
 
 import rest.award
+import statparser
 
 # Create an empty class to handle root directory requests
 class Root(object):
@@ -14,6 +15,7 @@ root.award = rest.award.Handler()
 
 # Get the path to the configuration file
 confPath = os.path.join(os.path.dirname(__file__), 'application.conf')
+current_dir = os.path.abspath(os.path.dirname(__file__))
 
 # Start the web application server
 if __name__ == '__main__':
