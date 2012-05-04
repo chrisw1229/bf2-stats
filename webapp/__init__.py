f6dacf47b45d3e07667652a7695e4cd847264ed0
Chris Ward <chrisw1229@gmail.com>
2012-05-04 09:04:20 -0400

    Minor cleanup.

--- webapp/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# Get the current directory for use in the configuration file
-import os.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Register the stat parser plugin
-import statparser
--- webapp/application.py
+++ webapp/application.py
@@ -2,8 +2,9 @@ import os.path
 
 import cherrypy
 
-import rest.award
+# Importing the stats parser plugin registers it with the cherrypy engine
 import statparser
+import rest.award
 
 # Create an empty class to handle root directory requests
 class Root(object):
@@ -13,9 +14,11 @@ root = Root()
 # Register all the REST request handlers
 root.award = rest.award.Handler()
 
+# The current directory is needed in the config file
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
 # Get the path to the configuration file
 confPath = os.path.join(os.path.dirname(__file__), 'application.conf')
-current_dir = os.path.abspath(os.path.dirname(__file__))
 
 # Start the web application server
 if __name__ == '__main__':
