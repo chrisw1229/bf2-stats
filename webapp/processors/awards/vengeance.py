afc1f9f945cc3d646afcd6c05ae213c231c3367d
Clinton Jenkins <cjenkins@robowarp.com>
2012-06-20 21:13:48 -0400

    Added award for Vengeance.

---
+++ webapp/processors/awards/vengeance.py
@@ -0,0 +1,27 @@
+from processors.awards import AwardProcessor,Column
+
+class Processor(AwardProcessor):
+    '''
+    Vengeance is awarded to the player who has the most kills vs. the player who has most recently killed
+    them.
+
+    The player who has most recently killed another player is only changed upon a valid kill.
+    '''
+
+    def __init__(self):
+        AwardProcessor.__init__(self, 'Vengeance', 'Most Kills of Person Who Most Recently Killed You', [
+                Column('Players'), Column('Kills', Column.NUMBER, Column.DESC)])
+
+        self.last_killer = dict()
+
+    def on_kill(self, e):
+
+        # Ignore suicides and team kills
+        if not e.valid_kill:
+            return
+
+        self.last_killer[e.victim] = e.attacker
+
+        if e.attacker in self.last_killer.keys() and e.victim == self.last_killer[e.attacker]:
+            self.results[e.attacker] += 1
+
