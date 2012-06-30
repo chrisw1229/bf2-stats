374b333b8fc19287f885f98be09b70088cc168d9
Chris Ward <chrisw1229@gmail.com>
2012-06-30 18:51:19 -0400

    Added an empty awards page with navigation menu.

---
+++ webapp/www/scripts/page-awards.js
@@ -0,0 +1,13 @@
+﻿// Handles all the controller logic for the awards page
+$(function() {
+
+// Register the page manager as a jQuery extension
+$.extend({ mgr: {
+
+}});
+
+// Load the custom jQuery user interface components
+$('.logger-widget').logger();
+$('.nav-widget').nav();
+
+});
---
+++ webapp/www/scripts/ui-nav.js
@@ -0,0 +1,70 @@
+﻿(function($, undefined) {
+
+$.widget('ui.nav', {
+
+   // Configure the default widget options
+   options: {
+      header: 'Menu',
+      footer: '08-25-2012',
+      icon: 'ui-icon-triangle-1-e',
+      menus: [
+         { name: 'Achievements', tip: '', url: 'achievements.html' },
+         { name: 'Awards', tip: '', url: 'awards.html' },
+         { name: 'Games', tip: '', url: 'games.html' },
+         { name: 'Kits', tip: '', url: 'kits.html' },
+         { name: 'Leaderboard', tip: '', url: 'leaderboard.html' },
+         { name: 'Maps', tip: '', url: 'maps.html' },
+         { name: 'Matchups', tip: '', url: 'matchups.html' },
+         { name: 'Players', tip: '', url: 'players.html' },
+         { name: 'Vehicles', tip: '', url: 'vehicles.html' },
+         { name: 'Weapons', tip: '', url: 'weapons.html' }
+      ]
+   },
+
+   _create: function() {
+
+      // Build the document model
+      this.element.addClass('ui-nav');
+      this.contentElm = $('<div class="ui-widget-content ui-corner-all ui-nav-content"/>').appendTo(this.element);
+      this.logoElm = $('<div class="ui-nav-logo">LOGO</div>').appendTo(this.contentElm);
+
+      // Build the menu header
+      this.headerDiv = $('<h2 class="ui-helper-reset ui-widget-header ui-corner-top ui-nav-menu-header">'
+            + this.options.header + '</h2>').appendTo(this.contentElm);
+
+      // Build the menu item elements
+      this.menusElm = $('<div class="ui-nav-menus"/>').appendTo(this.contentElm);
+      for (var i = 0; i < this.options.menus.length; i++) {
+         var menu = this.options.menus[i];
+         var menuElm = $('<h3 class="ui-helper-reset ui-state-default ui-nav-menu"/>').appendTo(this.menusElm);
+         $('<span class="ui-icon ' + this.options.icon + ' ui-nav-menu-icon"/>').appendTo(menuElm);
+         $('<a class="ui-nav-menu-link" href="' + menu.url + '" title="' + menu.tip + '">'
+               + menu.name + '</a>').appendTo(menuElm);
+      }
+
+      // Build the menu footer
+      this.footerElm = $('<h4 class="ui-helper-reset ui-widget-header ui-corner-bottom ui-nav-menu-footer">'
+            + this.options.footer + '</h4>').appendTo(this.contentElm);
+
+      // Bind the event handlers
+      this.menusElm.on('mouseenter', '.ui-nav-menu',
+            function(e) { $(this).addClass('ui-state-hover'); });
+      this.menusElm.on('mouseleave', '.ui-nav-menu',
+            function(e) { $(this).removeClass('ui-state-hover'); });
+   },
+
+   destroy: function() {
+
+      // Clear the event handlers
+      $('ui-nav-menu', this.menusElm).off('mouseenter mouseleave');
+
+      // Destroy the document model
+      this.contentElm.remove();
+      this.element.removeClass('ui-nav')
+
+      $.Widget.prototype.destroy.call(this);
+   }
+
+});
+
+}(jQuery));
