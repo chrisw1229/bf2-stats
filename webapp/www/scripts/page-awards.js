0027bd3d03e711c388e4869d52063c74976cbe7d
Chris Ward <chrisw1229@gmail.com>
2012-07-03 09:37:18 -0400

    Added a list of awards page.

--- webapp/www/scripts/page-awards.js
+++ webapp/www/scripts/page-awards.js
@@ -3,19 +3,25 @@ $(function() {
 
 var awardsElm = $('.awards-content');
 var awardElm = $('.award-content');
-
-var tableElm = $('.table-widget').table();
+var listElm = $('.list-widget');
+var tableElm = $('.table-widget');
 
 // Register the page manager as a jQuery extension
 $.extend({ mgr: {
 
    onHistory: function(e) {
+
+      // Get the award identifier if available
       var id = e.getState('id');
       if (id) {
+
+         // Show the individual award screen
          awardsElm.hide();
          awardElm.show();
          $.mgr.requestAward(id);
       } else {
+
+         // Show the awards list screen
          awardElm.hide();
          awardsElm.show();
          $.mgr.requestAwards();
@@ -23,10 +29,27 @@ $.extend({ mgr: {
    },
 
    requestAwards: function() {
-      console.log('AWARDS');
+
+      // Display the loading indicator
+      listElm.list();
+      listElm.list('loading');
+
+      // Configure the request options
+      var options = {
+         url: 'services/awards',
+         dataType: 'json',
+         success: $.proxy($.mgr.onAwards, $.mgr),
+         error: $.proxy($.mgr.onError, $.mgr)
+      };
+
+      // Fetch the content for the selected award
+      $.ajax(options);
    },
 
    requestAward: function(id) {
+
+      // Display the loading indicator
+      tableElm.table();
       tableElm.table('loading');
 
       // Configure the request options
@@ -41,14 +64,26 @@ $.extend({ mgr: {
       $.ajax(options);
    },
 
+   onAwards: function(data) {
+
+      // Update the header
+      var headerElm = $('.common-header', awardsElm);
+      headerElm.text('Awards (' + data.length + ')');
+
+      // Populate the list with awards
+      listElm.list('setRows', data);
+   },
+
    onAward: function(data) {
-   
+
       // Update the header
       var headerElm = $('.common-header', awardElm);
       headerElm.text(data.name + ' Award');
-      $('.award-desc', awardElm).text(data.desc);
-      $('.award-notes', awardElm).text(data.notes);
 
+      // Update the award attributes
+      $('.award-desc', awardElm).text(data.desc);
+
+      // Populate the table with award results
       tableElm.table('setColumns', data.columns);
       tableElm.table('setRows', data.rows);
    },
---
+++ webapp/www/scripts/ui-list.js
@@ -0,0 +1,102 @@
+﻿(function($, undefined) {
+
+$.widget('ui.list', {
+
+   // Configure the default widget options
+   options: {
+
+   },
+
+   _create: function() {
+
+      // Build the document model
+      this.element.addClass('ui-list');
+      this.listElm = $('<ol class="ui-list-items"/>').appendTo(this.element);
+
+      // Create the status message rows
+      this.emptyElm = $('<div class="ui-state-active ui-list-empty"><span class="ui-icon ui-icon-alert"/>No Records Available</div>');
+      this.loadElm = $('<div class="ui-state-active ui-list-load"><span class="ui-icon"/>Loading...</div>');
+      this.errorElm = $('<div class="ui-state-error ui-list-error"><span class="ui-icon ui-icon-alert"/>ERROR - Records Not Found</div>');
+
+      // Bind event handlers for the list items
+      this.listElm.on('mouseenter', '.ui-list-row',
+            function(e) { $(this).addClass('ui-state-hover'); });
+      this.listElm.on('mouseleave', '.ui-list-row',
+            function(e) { $(this).removeClass('ui-state-hover'); });
+   },
+
+   destroy: function() {
+
+      // Destroy the document model
+      this.emptyElm.remove();
+      this.loadElm.remove();
+      this.errorElm.remove();
+      this.listElm.remove();
+      this.element.removeClass('ui-list');
+
+      $.Widget.prototype.destroy.call(this);
+   },
+
+   setRows: function(rows) {
+      rows = ($.isArray(rows) ? rows : [rows]);
+
+      // Clear the current table data
+      this.clear();
+
+      // Add all the new rows to the model
+      this.rows = this.rows.concat(rows);
+
+      // Fill the table elements with row values
+      for (var i = 0; i < this.rows.length; i++) {
+         this._displayRow(i, this.rows[i]);
+      }
+
+      // Remove the loading and empty list messages
+      if (rows.length > 0) {
+         this.emptyElm.remove();
+         this.loadElm.remove();
+      }
+   },
+
+   clear: function() {
+      this.rows = [];
+      $('.ui-list-row', this.bodyElm).remove();
+
+      // Display the empty table message
+      this.loadElm.remove();
+      this.errorElm.remove();
+      this.emptyElm.appendTo(this.element);
+   },
+
+   loading: function() {
+      this.clear();
+      this.emptyElm.remove();
+      this.errorElm.remove();
+      this.loadElm.appendTo(this.element);
+   },
+
+   error: function() {
+      this.clear();
+      this.emptyElm.remove();
+      this.loadElm.remove();
+      this.errorElm.appendTo(this.element);
+   },
+
+   _displayRow: function(index, item) {
+
+      // Create a new table row to store the data
+      var sequence = (index % 2 == 0 ? 'even' : 'odd');
+      var rowElm = $('<li class="ui-widget ui-state-default'
+            + ' ui-list-row ui-list-row-' + sequence
+            + (index == 0 ? ' ui-corner-top' : '')
+            + (index == this.rows.length - 1 ? ' ui-corner-bottom' : '')
+            + '"/>').appendTo(this.listElm);
+
+      var linkElm = $('<a class="ui-list-item-link" href="#id=' + item.id + '"/>').appendTo(rowElm);
+      var nameElm = $('<span class="ui-list-item-name">' + item.name + '</span>').appendTo(linkElm);
+      var descElm = $('<span class="ui-list-item-desc">' + item.desc + '</span>').appendTo(linkElm);
+   }
+
+});
+
+}(jQuery));
