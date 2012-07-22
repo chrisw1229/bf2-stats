f01fc3dc387118f1ff0b1557ec48aabc44f2c284
John Tobe <john@tobe.name>
2012-07-22 15:49:37 -0400

    Tweaking the blindspot height check code

--- webapp/processors/awards/blindspot.py
+++ webapp/processors/awards/blindspot.py
@@ -19,11 +19,18 @@ class Processor(AwardProcessor):
     def __init__(self):
         AwardProcessor.__init__(self, 'Blindspot', 'Most Aircraft Deaths from Buildings', [
                 Column('Players'), Column('Kills', Column.NUMBER, Column.DESC)])
+
+        self.startPos = dict()
+
+    def on_vehicle_enter(self, e):
+
+        self.startPos[e.player] = e.player_pos[1]
 		
     def on_kill(self, e):
 
 
         type = e.vehicle.vehicle_type;
         if type == HELICOPTER or type == JET:
-            if e.attacker is None and e.victim_pos[1] > 100:
+            height = e.victim_pos[1] - self.startPos[e.player]
+            if e.attacker is None and height > 100:''' need to playtest the height'''
                 self.results[e.victim] += 1
