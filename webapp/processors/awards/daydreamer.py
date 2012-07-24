0e5f3551db729cda5a5ca7ea41d707cec60ee99c
Chris Ward <chrisw1229@gmail.com>
2012-07-24 15:18:23 -0400

    Fixed some logic problems in the daydreamer award.

--- webapp/processors/awards/daydreamer.py
+++ webapp/processors/awards/daydreamer.py
@@ -21,29 +21,46 @@ class Processor(AwardProcessor):
                 Column('Players'), Column('Time', Column.TIME, Column.DESC)])
 
         # Setup the results to store timers instead of numbers
-        self.timer = dict()
-
         self.results = dict()
 
-    def on_kill(self, e):
-
-        if not e.valid_kill:
-            return
-
-        # Create a timer for the player as needed
-        if not e.attacker in self.timer:
-            self.timer[e.attacker] = Timer()
-            self.results[e.attacker] = Timer()
-
-        # Get the time since the last kill and compare to current max time
-        if self.timer[e.attacker].running:
-            self.results[e.attacker] = max(self.results[e.attacker].elapsed, self.timer[e.attacker].elapsed)
-            self.timer[e.attacker].reset()
-
-        # Start the timer for new kill
-        self.results[e.attacker].start(e.tick)
+        # Store temporary timers for each player
+        self.timers = dict()
 
     def on_death(self, e):
 
-        # Stop the timer for the player
-        self.timer[e.player].reset()
+        # Reset the timer for the player
+        if e.player in self.timers:
+            self.timers[e.player].reset()
+
+    def on_disconnect(self, e):
+
+        # Reset the timer for the player
+        if e.player in self.timers:
+            self.timers[e.player].reset()
+
+    def on_kill(self, e):
+
+        # Ignore team kills and suicides
+        if not e.valid_kill:
+            return
+
+        # Create timers for the attacker as needed
+        if not e.attacker in self.results:
+            self.results[e.attacker] = Timer()
+            self.timers[e.attacker] = Timer()
+
+        # Check whether the attacker has killed previously for the current life
+        if self.timers[e.attacker].running:
+
+            # Stop the timer for the current kill
+            self.timers[e.attacker].stop(e.tick)
+
+            # Swap timers if the time difference is the new maximum
+            if self.timers[e.attacker].elapsed > self.results[e.attacker].elapsed:
+                temp_timer = self.results[e.attacker]
+                self.results[e.attacker] = self.timers[e.attacker]
+                self.timers[e.attacker] = temp_timer
+
+        # Reset the attacker timer for the next kill
+        self.timers[e.attacker].reset()
+        self.timers[e.attacker].start(e.tick)
