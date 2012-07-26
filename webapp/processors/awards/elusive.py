8925e7642f1dea884c122cc67c6c93dfdef27a78
John Tobe <john@tobe.name>
2012-07-25 21:40:59 -0400

    Adding elusive award

---
+++ webapp/processors/awards/elusive.py
@@ -0,0 +1,24 @@
+from processors.awards import AwardProcessor,Column
+
+class Processor(AwardProcessor):
+    '''
+    Overview
+    This processor keeps track of the person with the least kills and deaths
+
+    Implementation
+	Whenever a kill or death event is received it's cached
+    Notes
+	None.
+    '''
+
+    def __init__(self):
+        AwardProcessor.__init__(self, 'Elusive', 'Fewest Kills and Deaths', [
+                Column('Players'), Column('Kills/Deaths', Column.NUMBER, Column.ASC)])
+		
+    def on_kill(self, e):
+        # Ignore suicides and team kills
+        if not e.valid_kill:
+            return
+
+        self.results[e.attacker] += 1
+        self.results[e.victim] += 1
