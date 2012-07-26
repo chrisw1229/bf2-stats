f6a69b856a9d250d28852e55c93e512f3b567cf7
John Tobe <john@tobe.name>
2012-07-25 22:07:29 -0400

    Adding endurance award

---
+++ webapp/processors/awards/endurance.py
@@ -0,0 +1,26 @@
+
+from processors.awards import AwardProcessor,Column
+from timer import Timer
+
+class Processor(AwardProcessor):
+    '''
+    Overview
+    This processor keeps track of the amount of time a player is playing.
+    
+    Implementation
+    This implementation uses the timer object to automatically compute elapsed
+    time based on game ticks as events are processed.
+
+    Notes
+    None.
+    '''
+
+    def __init__(self):
+        AwardProcessor.__init__(self, 'Commander In Chief',
+                'Most Time As Commander', [
+                Column('Players'), Column('Time', Column.TIME, Column.DESC)])
+
+    def on_spawn(self, e):
+
+        player_stats = stat_mgr.get_player_stats(e.player)
+        self.results[e.player] = player_stats.play_time
