84b9267ff317d12476bbb5495eb996b92006fa1c
Chris Ward <chrisw1229@gmail.com>
2012-08-02 01:59:55 -0400

    Added genghis khan award.

---
+++ webapp/processors/awards/genghis_khan.py
@@ -0,0 +1,33 @@
+
+from processors.awards import AwardProcessor,Column
+from models import model_mgr
+
+class Processor(AwardProcessor):
+    '''
+    Overview
+    This processor keeps track of the number of kills while commander
+
+    Implementation
+    Increment the kill count for the commander for each kill
+
+    Notes
+    None.
+    '''
+
+    def __init__(self):
+        AwardProcessor.__init__(self, 'Genghis Khan',
+                'Most Subordinate Kills as Commander', [
+                Column('Players'), Column('Kills', Column.NUMBER, Column.DESC)])
+
+    def on_kill(self, e):
+
+        # Ignore team kills and suicides
+        if not e.valid_kill:
+            return
+
+        # Get the commander for the attacker's team
+        team = model_mgr.get_team(e.attacker.team_id)
+        commander = model_mgr.get_player(team.commander_id)
+
+        # Give a point to the commander
+        self.results[commander] += 1
