8d2c3a47ab09c32e3bfdf2726aae76539554c633
Chris Ward <chrisw1229@gmail.com>
2012-08-03 11:48:15 -0400

    Removed c4 weapon from drunk driver award.

--- webapp/processors/awards/drunk_driver.py
+++ webapp/processors/awards/drunk_driver.py
@@ -1,5 +1,4 @@
 from processors.awards import AwardProcessor,Column
-from models.weapons import MINE
 
 class Processor(AwardProcessor):
     '''
@@ -12,5 +11,6 @@ class Processor(AwardProcessor):
                 Column('Players'), Column('Deaths', Column.NUMBER, Column.DESC)])
 
     def on_kill(self, e):
-        if e.victim.driver and e.weapon.weapon_type == MINE:
-            self.results[e.victim] += 1
+        if e.victim.driver:
+            if e.weapon.id == 'at_mine' or e.weapon.id == 'usmin_claymore':
+                self.results[e.victim] += 1
