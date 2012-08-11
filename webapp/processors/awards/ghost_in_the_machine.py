0a9739b5f351b914a4fd31bf31cfa6b9078e75c2
John Tobe <john@tobe.name>
2012-08-10 21:18:39 -0400

    Ghost in the machine

---
+++ webapp/processors/awards/ghost_in_the_machine.py
@@ -0,0 +1,31 @@
+
+from processors.awards import AwardProcessor,Column
+from models import model_mgr
+from stats import stat_mgr
+
+class Processor(AwardProcessor):
+    '''
+    Overview
+    This processor keeps track of the number of vehicles destroyed near a player
+
+    Implementation
+    When a vehicle is destroyed, find the distance of each player's current
+    position to the destroyed vehicle and increment their count if less than 5 meters
+
+    Notes
+    None.
+    '''
+
+    def __init__(self):
+        AwardProcessor.__init__(self, 'Ghost in the Machine',
+                'Most Vehicle Deaths Near Player', [
+                Column('Players'), Column('Vehicles', Column.NUMBER, Column.DESC)])
+        
+    def on_vehicle_destroy(self, e):
+
+        vpos = e.vehicle_pos
+
+        for player in model_mgr.get_players():
+            dist = stat_mgr.dist_3d( vpos, player.pos )
+            if dist < 5:
+                self.results[player] += 1
