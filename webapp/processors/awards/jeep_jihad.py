7c812b1765a3133cfb2a0b7d0f6da5d6a94f2eeb
John Tobe <john@tobe.name>
2012-08-13 00:48:18 -0400

    Fixes for Jeep Jihad and Launchpad awards

--- webapp/processors/awards/jeep_jihad.py
+++ webapp/processors/awards/jeep_jihad.py
@@ -1,26 +1,38 @@
 from processors.awards import AwardProcessor,Column
+from events import event_mgr
 
 class Processor(AwardProcessor):
     '''
     Overview
     This award is given to the player with the most kills by c4 loaded jeeps.
+
+    Implementation
+    On vehicle destroy, track the time and add kills from c4 that occurred at the
+    same time in subsequent kill events.  Also go through the history for the attacker
+    and add any c4 kills in his recent history.
     '''
 
     def __init__(self):
         AwardProcessor.__init__(self, 'Jeep Jihad', 'Most Kills by C4 Loaded Jeeps', [
                 Column('Players'), Column('Kills', Column.NUMBER, Column.DESC)])
 
-        self.exit_times = dict()
+        self.time = dict()
 
-    def on_vehicle_exit(self, e):
-        self.exit_times[e.player] = e.tick
+    def on_vehicle_destroy(self, e):
+
+        if e.attacker == e.driver:
+            self.time[e.attacker] = e.tick
+            recent = event_mgr.get_history(e.attacker).new_events
+            for event in recent:
+                if event.TYPE == 'KL':
+                    if event.valid_kill and event.weapon.id == 'c4_explosives':
+                        self.results[e.attacker] += 1
 
     def on_kill(self, e):
         #Ignore suicides and team kills
         if not e.valid_kill:
             return
 
-        if e.attacker in self.exit_times and e.weapon.id == 'c4_explosives':
-            #need to experiment with the time limit, probably need to reduce this
-            if e.tick - self.exit_times[e.attacker] <= 2:
+        if e.attacker in self.time and e.weapon.id == 'c4_explosives':
+            if e.tick == self.time[e.attacker]:
                 self.results[e.attacker] += 1
--- webapp/processors/awards/launchpad.py
+++ webapp/processors/awards/launchpad.py
@@ -2,8 +2,7 @@
 from processors.awards import AwardProcessor,Column
 from models import model_mgr
 from models.vehicles import AIR
-
-# TODO
+from models.players import EMPTY
 
 class Processor(AwardProcessor):
     '''
@@ -21,15 +20,23 @@ class Processor(AwardProcessor):
         AwardProcessor.__init__(self, 'Launchpad', 'Most Passenger Kills as Pilot', [
                 Column('Players'), Column('Kills', Column.NUMBER, Column.DESC)])
 
+        self.times = dict()
+        self.drivers = dict()
+        
+    def on_vehicle_destroy(self, e):
+        
+        if e.attacker == EMPTY and e.driver != EMPTY:
+            self.times[e.vehicle] = e.tick
+            self.drivers[e.vehicle] = e.driver
+            
     def on_kill(self, e):
 
         # Ignore kills where victim isn't passenger
         if not e.victim.passenger:
             return
 
-        victim_vehicle = model_mgr.get_vehicle(e.victim.vehicle_id)
-        if victim_vehicle.group == AIR:
-            for player in model_mgr.get_players():
-                if player.vehicle_id == e.victim.vehicle_id:
-                    if player.driver:
-                        self.results[player] += 1
+        v = model_mgr.get_vehicle(e.victim.vehicle_id)
+        if v.group == AIR:
+            if v in self.times and v in self.drivers and self.times[v] == e.tick:
+                driver = self.drivers[v]
+                self.results[driver] += 1
