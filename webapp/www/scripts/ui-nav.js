1098594bb2ee4a4f852b4bec759b87b056d3d740
Chris Ward <chrisw1229@gmail.com>
2012-08-21 02:43:54 -0400

    Added a logo.

--- webapp/www/scripts/ui-nav.js
+++ webapp/www/scripts/ui-nav.js
@@ -25,7 +25,7 @@ $.widget('ui.nav', {
       // Build the document model
       this.element.addClass('ui-nav');
       this.contentElm = $('<div class="ui-nav-content"/>').appendTo(this.element);
-      this.logoElm = $('<div class="ui-nav-logo">LOGO</div>').appendTo(this.contentElm);
+      this.logoElm = $('<img class="ui-nav-logo" src="images/logo.png"/>').appendTo(this.contentElm);
 
       // Build the menu header
       this.headerDiv = $('<h2 class="ui-helper-reset ui-widget-header ui-corner-top ui-nav-menu-header">'
