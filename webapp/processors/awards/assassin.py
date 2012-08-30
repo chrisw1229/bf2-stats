12a68be6e0015b69f6d23cb8ea7867291b0bc11a
Chris Ward <chrisw1229@gmail.com>
2012-08-30 00:44:35 -0400

    Fixed assassin award to count commanders as 1 point.

--- webapp/processors/awards/assassin.py
+++ webapp/processors/awards/assassin.py
@@ -16,15 +16,13 @@ class Processor(AwardProcessor):
         AwardProcessor.__init__(self, 'Assassin',
                 'Most Kills Against Commanders and Squad Leaders',
                 [PLAYER_COL, Column('Kills', Column.NUMBER, Column.DESC)])
-        
+
     def on_kill(self, e):
 
         # Ignore suicides and team kills
         if not e.valid_kill:
             return
-            
-        # +5 for commanders
-        if e.victim.commander:
-            self.results[e.attacker] += 5
-        elif e.victim.leader:
+
+        # Give a point when the victim is commander or leader
+        if e.victim.commander or e.victim.leader:
             self.results[e.attacker] += 1
