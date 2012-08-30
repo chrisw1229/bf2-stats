3f967c14ac5bf1fe0698cbf48e0f19c0d0cfc304
Chris Ward <chrisw1229@gmail.com>
2012-08-30 00:45:21 -0400

    Fixed bug splat to ignore vehicle weapons.

--- webapp/processors/awards/bug_splat.py
+++ webapp/processors/awards/bug_splat.py
@@ -2,6 +2,7 @@
 from processors.awards import AwardProcessor,Column,PLAYER_COL
 from models import model_mgr
 from models.vehicles import HELICOPTER, JET, PARACHUTE
+from models import weapons
 
 class Processor(AwardProcessor):
     '''
@@ -18,13 +19,17 @@ class Processor(AwardProcessor):
         AwardProcessor.__init__(self, 'Bug Splat',
                 'Most Kills Against Parachuters with an Aircraft',
                 [PLAYER_COL, Column('Kills', Column.NUMBER, Column.DESC)])
-        
+
     def on_kill(self, e):
 
         # Ignore suicides and team kills
         if not e.valid_kill:
             return
-            
+
+        # Hitting the victim with an aircraft would not have a weapon
+        if e.weapon != weapons.EMPTY:
+            return
+
         victim_vehicle = model_mgr.get_vehicle(e.victim.vehicle_id)
         if victim_vehicle.vehicle_type == PARACHUTE:
             if (e.vehicle.vehicle_type == JET
