d0e0b3eeb32082d5cfcb9ebbc461349ae6a5c88b
Chris Ward <chrisw1229@gmail.com>
2012-08-30 02:38:46 -0400

    Fixed rank awards to exclude artillery.

--- webapp/processors/awards/underdog.py
+++ webapp/processors/awards/underdog.py
@@ -1,5 +1,7 @@
+
 from processors.awards import AwardProcessor,Column,PLAYER_COL
 from stats import stat_mgr
+from models.weapons import ARTILLERY
 
 class Processor(AwardProcessor):
     '''
@@ -13,8 +15,9 @@ class Processor(AwardProcessor):
                 [PLAYER_COL, Column('Kills', Column.NUMBER, Column.DESC)])
 
     def on_kill(self, e):
-        attacker_stats = stat_mgr.get_player_stats(e.attacker)
-        victim_stats = stat_mgr.get_player_stats(e.victim)
+        if e.weapon.weapon_type != ARTILLERY:
+            attacker_stats = stat_mgr.get_player_stats(e.attacker)
+            victim_stats = stat_mgr.get_player_stats(e.victim)
 
-        if attacker_stats.rank <= 1 and victim_stats.rank >= 3:
-            self.results[e.attacker] += 1
+            if attacker_stats.rank <= 1 and victim_stats.rank >= 3:
+                self.results[e.attacker] += 1
