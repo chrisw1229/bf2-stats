5934cf295792517d09b496ec70fcd7c3a62f0dfb
John Tobe <john@tobe.name>
2012-09-01 23:13:01 -0400

    Fixing flip flop bugs

--- webapp/processors/awards/flip_flop.py
+++ webapp/processors/awards/flip_flop.py
@@ -15,7 +15,25 @@ class Processor(AwardProcessor):
     def __init__(self):
         AwardProcessor.__init__(self, 'Flip Flop', 'Most Team Changes',
                 [PLAYER_COL, Column('Teams', Column.NUMBER, Column.DESC)])
-		
+
+	self.lastTeam = dict()
+
+##    def on_spawn(self, e):
+##
+##        if e.player.name == '6MTZHP':
+##            print 'spawn'
+##            print e.tick
+            
     def on_team(self, e):
-        self.results[e.player] += 1
-                        
+
+##        if e.player.name == '6MTZHP':
+##            print 'team'
+##            print e.tick
+            
+        if e.player not in self.lastTeam:
+            self.lastTeam[e.player] = e.team
+            return
+
+        if e.team != self.lastTeam[e.player]:
+            self.lastTeam[e.player] = e.team
+            self.results[e.player] += 1
