39f2c6a39edffab9d555e69a409f2aed79c0a8b4
Chris Ward <chrisw1229@gmail.com>
2012-09-04 21:17:59 -0400

    Fixed weapon tester award to not include initial spawn weapon.

--- webapp/processors/awards/weapon_tester.py
+++ webapp/processors/awards/weapon_tester.py
@@ -3,12 +3,24 @@ from processors.awards import AwardProcessor,Column,PLAYER_COL
 class Processor(AwardProcessor):
     '''
     Overview
-    This award is given to the player with the most weapon changes.
+    This award is given to the player with the most weapon changes other than
+    the one they spawn with.
     '''
 
     def __init__(self):
         AwardProcessor.__init__(self, 'Weapon Tester', 'Most Weapon Changes',
                 [PLAYER_COL, Column('Weapon Changes', Column.NUMBER, Column.DESC)])
 
+        self.pickups = dict()
+
     def on_weapon(self, e):
-        self.results[e.player] += 1
+        if not e.player in self.pickups:
+            return
+
+        if self.pickups[e.player]:
+            self.results[e.player] += 1
+        else:
+            self.pickups[e.player] = True
+
+    def on_spawn(self, e):
+        self.pickups[e.player] = False
